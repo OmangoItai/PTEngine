f12dd6951365978a5e290f9b53dceafeeacf9463
おまんご痛い <972809723@qq.com>
2024-03-11 21:29:53 +0800

    update

--- unit.py
+++ unit.py
@@ -1,13 +1,13 @@
+from typing import Dict, List
 import numpy as np
 import functools
 import taskQueue
 class Unit:
     id: int
     name: str
-    produceTrigger: np.ufunc
-    consumeTrigger: np.ufunc
+    triggers: Dict[str: np.ufunc]
     inventory: np.ndarray
-    personList: list[int]
+    personList: List[int]
     # todo personList 是记编号还是引用
     
     def __init__(self, id: int, name: str, inventory: np.ndarray | list[float], personList: list[int]) -> None:
@@ -15,10 +15,7 @@ class Unit:
         self.name = name
         self.inventory = np.array(inventory) if isinstance(inventory,list) else inventory
         self.personList = personList
-        self.produceTrigger = None
+        self.produceTrigger = {}
     
-    def setProduceTrigger(self, func) -> taskQueue.Task:
-        self.produceTrigger = np.frompyfunc(functools.partial(func, self), 1, 1)
-
-    def setConsumeTrigger(self, func):
-        self.consumeTrigger = np.frompyfunc(functools.partial(func, self), 1, 1)
+    def setTrigger(self, name, func) -> taskQueue.Task:
+        self.triggers[name] = np.frompyfunc(functools.partial(func, self), 1, 1)
